9ab7a3b48e1225914b81958a4dab36f117be26a1
Arkreuz <Developer@arkreuz.com>
2020-12-19 01:36:13 -0600

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Zarco Dionisio'
+SITENAME = 'Mictlan Forensic'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['img']
+
+TIMEZONE = 'Mexico/General'
+
+DEFAULT_LANG = 'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Facebook', 'https://www.facebook.com/MictlanForensic'),
+         ('Instagram', 'https://www.instagram.com/mictlanforensic'),
+         ('Twitter', 'https://twitter.com/MictlanForensic'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
